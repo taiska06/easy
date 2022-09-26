2b04df8e585700ebf09e7f42d13f4d8b9264a2b1
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-09-26 14:20:10 +0300

    Update easy.h

--- easy.h
+++ easy.h
@@ -6,4 +6,9 @@
  int itc_abc(int num);
  double itc_fabs(double num);
  int itc_revnbr(int num);
+ int itc_max(int a,int b);
+ int itc_min(int a, int b);
+ double itc_fmax(double a, double b);
+ double itc_fmin(double a, double b);
+ int itc_sqrt(int num);
 #endif // EASY_H_INCLUDED
